e540a2739761de48f1cea7643e5df1c7bfc549b8
Dmytro <d.ogirenko@urancompany.com>
2018-08-31 22:36:33 +0300

    initial commit

---
+++ app/src/main/java/news/reddit/com/redditnews/NewsListActivity.kt
@@ -0,0 +1,12 @@
+package news.reddit.com.redditnews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_list)
+    }
+}
