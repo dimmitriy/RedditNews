a03d991990ec48d1aabf099e148b7b1c71b193b1
Dmytro <d.ogirenko@urancompany.com>
2018-08-31 23:19:23 +0300

    implement presenter, view

--- app/src/main/java/news/reddit/com/redditnews/NewsListActivity.kt
+++ app/src/main/java/news/reddit/com/redditnews/NewsListActivity.kt
@@ -3,10 +3,19 @@ package news.reddit.com.redditnews
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class NewsListActivity : AppCompatActivity() {
+class NewsListActivity : AppCompatActivity(), NewsListConract.View {
+
+    lateinit var presenter: NewsListPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_list)
+        presenter = NewsListPresenter()
+        presenter.onGetNewsTriggered()
     }
+
+    override fun onShowNewsList() {
+
+    }
+
 }
---
+++ app/src/main/java/news/reddit/com/redditnews/NewsListPresenter.kt
@@ -0,0 +1,9 @@
+package news.reddit.com.redditnews
+
+class NewsListPresenter: NewsListConract.Presenter {
+
+    override fun onGetNewsTriggered() {
+
+    }
+
+}
