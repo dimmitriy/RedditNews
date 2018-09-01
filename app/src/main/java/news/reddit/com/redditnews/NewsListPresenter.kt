87078509751736c78ad4fdca4db4f831b5d1bdf4
Dmytro <d.ogirenko@urancompany.com>
2018-09-01 10:01:41 +0300

    add repository to presenter

--- app/src/main/java/news/reddit/com/redditnews/NewsListPresenter.kt
+++ app/src/main/java/news/reddit/com/redditnews/NewsListPresenter.kt
@@ -1,9 +1,37 @@
 package news.reddit.com.redditnews
 
-class NewsListPresenter: NewsListConract.Presenter {
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import news.reddit.com.redditnews.model.NewsListRepository
+
+class NewsListPresenter(private val repository: NewsListRepository): NewsListContract.Presenter {
+
+    lateinit var view: NewsListContract.View
+    private val disposable = CompositeDisposable()
 
     override fun onGetNewsTriggered() {
+        disposable.add(
+                repository.getNews()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .unsubscribeOn(Schedulers.io())
+                        .doOnSubscribe { view.showProgress() }
+                        .doAfterTerminate { view.hideProgress() }
+                        .subscribe({ response ->
+                            view.onShowNewsList(response.data.children)
+                        }, { throwable ->
+                            view.showMessage(throwable.message!!)
+                        })
+        )
+    }
 
+    override fun subscribe() {
+
+    }
+
+    override fun unsubscribe() {
+        disposable.dispose()
     }
 
 }
