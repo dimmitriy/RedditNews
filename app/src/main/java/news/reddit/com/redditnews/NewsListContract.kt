bab22ddf8ba4441813541eb4ed1fd2b06905e634
Dmytro <d.ogirenko@urancompany.com>
2018-09-01 10:02:06 +0300

    add baseView, basePresenter to contract

---
+++ app/src/main/java/news/reddit/com/redditnews/NewsListContract.kt
@@ -0,0 +1,21 @@
+package news.reddit.com.redditnews
+
+import com.example.moviereview.base.BasePresenter
+import com.example.moviereview.base.BaseView
+import news.reddit.com.redditnews.response.NewsEntity
+
+class NewsListContract {
+
+    interface View: BaseView {
+
+        fun onShowNewsList(newsList: List<NewsEntity>)
+
+    }
+
+    interface Presenter: BasePresenter {
+
+        fun onGetNewsTriggered()
+
+    }
+
+}
