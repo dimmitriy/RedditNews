f7ee993d2b0e121cdf5df44000c90aa7688dc483
Dmytro <d.ogirenko@urancompany.com>
2018-09-01 10:02:24 +0300

    implement baseView methods

--- app/src/main/java/news/reddit/com/redditnews/NewsListActivity.kt
+++ app/src/main/java/news/reddit/com/redditnews/NewsListActivity.kt
@@ -1,21 +1,40 @@
 package news.reddit.com.redditnews
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import news.reddit.com.redditnews.model.NewsListDataSource
+import news.reddit.com.redditnews.response.NewsEntity
 
-class NewsListActivity : AppCompatActivity(), NewsListConract.View {
+class NewsListActivity : AppCompatActivity(), NewsListContract.View {
 
     lateinit var presenter: NewsListPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_list)
-        presenter = NewsListPresenter()
+        presenter = NewsListPresenter(NewsListDataSource())
         presenter.onGetNewsTriggered()
     }
 
-    override fun onShowNewsList() {
+    override fun onShowNewsList(newsList: List<NewsEntity>) {
 
+    }
+
+    override fun showMessage(message: String) {
+
+    }
+
+    override fun showProgress() {
+
+    }
+
+    override fun hideProgress() {
+
+    }
+
+    override fun getViewContext(): Context? {
+        return this
     }
 
 }
