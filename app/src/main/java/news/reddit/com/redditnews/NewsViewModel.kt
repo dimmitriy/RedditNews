490ff39164cff3238f1854bc8907b50952d57ae2
Dmytro <d.ogirenko@urancompany.com>
2018-09-03 22:16:43 +0300

    add to viewModel news variable to get if data loaded

--- app/src/main/java/news/reddit/com/redditnews/NewsViewModel.kt
+++ app/src/main/java/news/reddit/com/redditnews/NewsViewModel.kt
@@ -3,11 +3,24 @@ package news.reddit.com.redditnews
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 
 import news.reddit.com.redditnews.response.NewsEntity
 
-class NewsViewModel(application: Application, repository: NewsListRepository) : AndroidViewModel(application) {
+class NewsViewModel(application: Application, private val repository: NewsListRepository) : AndroidViewModel(application) {
 
-    val news: LiveData<List<NewsEntity>> = repository.getNews("a")
+    private var newsData: LiveData<List<NewsEntity>>? = null
+
+    fun getNews(): LiveData<List<NewsEntity>> {
+        if (newsData == null){
+            newsData = MutableLiveData<List<NewsEntity>>()
+            loadNews()
+        }
+        return newsData as LiveData<List<NewsEntity>>
+    }
+
+    private fun loadNews() {
+        newsData = repository.getNews("a")
+    }
 
 }
